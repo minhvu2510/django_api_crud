9a06cb1afedd55c4cbfac1caf622e874e2671035
minhvu <vunguyenminh@vccorp.vn>
2021-01-26 17:06:45 +0700

    config ui

--- essential_words/settings.py
+++ essential_words/settings.py
@@ -150,5 +150,5 @@ STATICFILES_DIRS = (
 django_heroku.settings(locals())
 
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
