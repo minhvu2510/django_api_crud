72085c5295ae2cff3af709d1a82cb26511fe8f16
minhvu <vunguyenminh@vccorp.vn>
2021-01-26 16:13:08 +0700

    config ui

--- essential_words/settings.py
+++ essential_words/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, "static")
