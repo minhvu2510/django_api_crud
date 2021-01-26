1e45befe4c908301ca7ff47052e5eb4c06634aba
minhvu <vunguyenminh@vccorp.vn>
2021-01-26 16:55:07 +0700

    config ui

--- essential_words/wsgi.py
+++ essential_words/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'essential_words.settings')
+application = DjangoWhiteNoise(application)
 
 application = get_wsgi_application()
