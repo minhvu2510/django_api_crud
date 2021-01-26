5e998d02cbc490668a97cfb7b6b8a5400825a235
minhvu <nguyenminhvu2510@gmail.com>
2021-01-26 22:50:33 +0700

    Rec: rectify config

--- essential_words/settings.py
+++ essential_words/settings.py
@@ -149,14 +149,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-#     normpath(join(BASE_DIR, 'upload')),
-# )
-STATIC_ROOT = normpath(join(BASE_DIR, 'static'))
+# STATICFILES_DIRS = [
+#     os.path.join(PROJECT_ROOT, 'static')
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
 
+# STATICFILES_DIRS = [
+#     os.path.join(PROJECT_DIR, 'static'),
+# ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
