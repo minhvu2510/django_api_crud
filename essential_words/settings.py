2408dc99e9790723482c3a4421ad1c83be944ba0
minhvu <vunguyenminh@vccorp.vn>
2021-01-26 17:14:09 +0700

    config ui

--- essential_words/settings.py
+++ essential_words/settings.py
@@ -118,7 +118,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
